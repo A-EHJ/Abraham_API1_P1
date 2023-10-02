9dd19ab77f99627be8a9ed496274a670a780ff80
A-EHJ <abrahamelhage2003@gmail.com>
2023-10-02 18:32:19 -0400

    Creando Modelo Aporte

---
+++ Models/Aportes.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Abraham_API1_P1.Models
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteId { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la persona")]
+        public string Persona { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir la Observación")]
+        public string Observacion { get; set; }
+
+        [Required(ErrorMessage = "Es obligatorio introducir el Monto")]
+        [Range(1, double.MaxValue, ErrorMessage = "El monto debe ser mayor que cero")]
+        public double Monto { get; set; }
+
+    }
+}
