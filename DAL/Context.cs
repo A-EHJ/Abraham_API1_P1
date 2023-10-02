4538a7bc68069a9991e38d28f6fc1e589f9b7e72
A-EHJ <abrahamelhage2003@gmail.com>
2023-10-02 18:32:48 -0400

    Agregando al contexto el DbSet<Aporte>

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
-
+using Abraham_API1_P1.Models;
 namespace Abraham_API1_P1.DAL
 {
     public class Context : DbContext
     {
+        public DbSet<Aportes> Aportes { get; set; }
         public Context(DbContextOptions<Context> options) : base(options) { }
     }
 }
